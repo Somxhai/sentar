ea338ae0cf5ce0ce7c9bde4f3373e11177977295
= <ChanakanThongrong@gmail.com>
2025-10-05 14:57:34 +0700

    feat(backend): setup project

---
+++ backend/src/main.rs
@@ -0,0 +1,43 @@
+use axum::{Json, Router};
+use serde::Serialize;
+use std::net::SocketAddr;
+use tracing::error;
+use utoipa_axum::{router::OpenApiRouter, routes};
+use utoipa_swagger_ui::SwaggerUi;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    // Build router and OpenAPI spec
+    let (router, api): (Router, utoipa::openapi::OpenApi) =
+        OpenApiRouter::new().routes(routes!(ping)).split_for_parts();
+
+    // Merge Swagger UI route
+    let app = router.merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api));
+
+    // Start server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap_or_else(|err| error!("Cannot start the server: {}", err));
+}
+
+#[derive(Serialize, utoipa::ToSchema)]
+struct PingResponse {
+    message: String,
+}
+
+#[utoipa::path(
+    get,
+    path = "/",
+    responses(
+        (status = 200, description = "Ping successful", body = PingResponse)
+    )
+)]
+async fn ping() -> Json<PingResponse> {
+    Json(PingResponse {
+        message: "Ping!".to_string(),
+    })
+}
