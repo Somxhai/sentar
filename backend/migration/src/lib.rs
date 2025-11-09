925e0bb3eec592ce808eed0d8f8b5fb64f2f4f57
= <ChanakanThongrong@gmail.com>
2025-11-09 10:35:27 +0700

    feat(migration): add app tables

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
+mod m20251109_032731_app_schema;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20220101_000001_create_table::Migration),
+            Box::new(m20251109_032731_app_schema::Migration),
+        ]
     }
 }
