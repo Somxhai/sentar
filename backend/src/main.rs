54bdc503c9177cd97cd53b9d29d92e5c59cc1239
= <ChanakanThongrong@gmail.com>
2025-11-26 18:42:09 +0700

    feat(loki): add

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,13 @@
 use std::net::SocketAddr;
+use std::process;
 
 use backend::app::create_database;
 use eyre::Result;
 use tracing::error;
 use tracing::info;
+use tracing_loki::url::Url;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 use crate::app::create_router;
 
@@ -11,11 +15,21 @@ pub mod app;
 pub mod dto;
 pub mod error;
 pub mod model;
+pub mod prometheus;
 pub mod routes;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt().init();
+    let (layer, task) = tracing_loki::builder()
+        .label("host", "mine")?
+        .extra_field("pid", format!("{}", process::id()))?
+        .build_url(Url::parse("http://127.0.0.1:3100").unwrap())?;
+
+    tracing_subscriber::registry()
+        .with(layer)
+        .with(tracing_subscriber::fmt::Layer::new())
+        .init();
+    tokio::spawn(task);
     dotenv::dotenv().ok();
 
     let db = create_database().await?;
