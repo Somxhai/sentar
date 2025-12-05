825b2c813f330e02b01200d0afc06f9d3e4d90ff
= <ChanakanThongrong@gmail.com>
2025-12-05 23:58:24 +0700

    fix: oltp blocking logging

--- backend/src/observe.rs
+++ backend/src/observe.rs
@@ -1,20 +1,27 @@
 use std::process;
 
 use eyre::Result;
-use opentelemetry::global;
+use opentelemetry::global::{self, BoxedTracer};
+use opentelemetry_otlp::WithExportConfig;
 use tracing_loki::{BackgroundTask, Layer, url::Url};
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::Registry;
 
-pub fn create_oltp_provider() -> Result<()> {
+pub fn create_oltp_provider() -> Result<OpenTelemetryLayer<Registry, BoxedTracer>> {
     let otlp_exporter = opentelemetry_otlp::SpanExporter::builder()
         .with_tonic()
+        .with_endpoint("http://localhost:4317")
         .build()?;
 
     let provider = opentelemetry_sdk::trace::SdkTracerProvider::builder()
-        .with_simple_exporter(otlp_exporter)
+        .with_batch_exporter(otlp_exporter)
         .build();
 
     global::set_tracer_provider(provider);
-    Ok(())
+    let tracer = global::tracer("sentar_trace");
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    Ok(otel_layer)
 }
 
 pub fn create_logging_provider() -> Result<(Layer, BackgroundTask)> {
