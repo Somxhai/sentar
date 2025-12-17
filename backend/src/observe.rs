c2bd79e4624b263285bf0fd2cd4e0586744c71b6
= <ChanakanThongrong@gmail.com>
2025-12-17 16:45:49 +0700

    feat: improve observation

--- backend/src/observe.rs
+++ backend/src/observe.rs
@@ -1,24 +1,41 @@
 use std::process;
 
 use eyre::Result;
-use opentelemetry::global::{self, BoxedTracer};
-use opentelemetry_otlp::WithExportConfig;
+use opentelemetry::{
+    KeyValue,
+    global::{self, BoxedTracer},
+};
+use opentelemetry_otlp::{ExportConfig, WithExportConfig};
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::{RandomIdGenerator, Sampler};
 use tracing_loki::{BackgroundTask, Layer, url::Url};
 use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::Registry;
 
 pub fn create_oltp_provider() -> Result<OpenTelemetryLayer<Registry, BoxedTracer>> {
+    let resource = Resource::builder()
+        .with_service_name("sentar-backend")
+        .with_attribute(KeyValue::new("env", "development"))
+        .build();
+
     let otlp_exporter = opentelemetry_otlp::SpanExporter::builder()
         .with_tonic()
-        .with_endpoint("http://localhost:4317")
+        .with_export_config(ExportConfig {
+            endpoint: Some("http://localhost:4317".into()),
+            protocol: opentelemetry_otlp::Protocol::Grpc,
+            ..Default::default()
+        })
         .build()?;
 
     let provider = opentelemetry_sdk::trace::SdkTracerProvider::builder()
         .with_batch_exporter(otlp_exporter)
+        .with_resource(resource)
+        .with_sampler(Sampler::AlwaysOn)
+        .with_id_generator(RandomIdGenerator::default())
         .build();
 
-    global::set_tracer_provider(provider);
     let tracer = global::tracer("sentar_trace");
+    global::set_tracer_provider(provider);
     let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
 
     Ok(otel_layer)
