307491b493d92e1abd7a5659842b98c534b34972
= <ChanakanThongrong@gmail.com>
2026-01-04 10:24:26 +0700

    chore: remove info!

--- backend/src/middleware/connect_info.rs
+++ backend/src/middleware/connect_info.rs
@@ -1,7 +1,6 @@
 use axum::{extract::Request, middleware::Next, response::Response};
 use axum_client_ip::ClientIp;
 use axum_extra::{TypedHeader, headers::UserAgent};
-use tracing::info;
 
 use crate::dto::connection_info::ConnectionInfo;
 
@@ -12,7 +11,6 @@ pub async fn retrieve_connection_info_middleware(
     next: Next,
 ) -> Response {
     let connection_info = ConnectionInfo { ip, user_agent };
-    info!("connection_info: {:?}", connection_info);
     req.extensions_mut().insert(connection_info);
 
     next.run(req).await
