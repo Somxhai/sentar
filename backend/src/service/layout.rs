e94e4e37d19a7439e93136584e33450af1f79ca9
= <ChanakanThongrong@gmail.com>
2026-01-14 18:49:39 +0700

    fix: dependency issue

--- backend/src/service/layout.rs
+++ backend/src/service/layout.rs
@@ -1,8 +1,6 @@
-use crate::{error::AppError, model::event_object_position};
-use sea_orm::{
-    ActiveValue::Set, ColumnTrait, DatabaseConnection, EntityTrait, ExprTrait, QueryFilter,
-};
-use sea_query::Expr;
+use crate::model::event_object_position;
+use sea_orm::{ActiveValue::Set, DatabaseConnection, EntityTrait};
+// use sea_query::Expr;
 use uuid::Uuid;
 
 #[allow(dead_code)]
@@ -42,31 +40,31 @@ impl LayoutManagement {
         Ok(model)
     }
 
-    #[allow(dead_code)]
-    pub async fn move_objects(
-        self,
-        seat_ids: Vec<Uuid>,
-        delta_x: f64,
-        delta_y: f64,
-        delta_z: f64,
-    ) -> Result<Vec<event_object_position::Model>, AppError> {
-        let result = event_object_position::Entity::update_many()
-            .col_expr(
-                event_object_position::Column::PositionX,
-                Expr::col(event_object_position::Column::PositionX).add(delta_x),
-            )
-            .col_expr(
-                event_object_position::Column::PositionY,
-                Expr::col(event_object_position::Column::PositionY).add(delta_y),
-            )
-            .col_expr(
-                event_object_position::Column::PositionZ,
-                Expr::col(event_object_position::Column::PositionZ).add(delta_z),
-            )
-            .filter(event_object_position::Column::Id.is_in(seat_ids))
-            .exec_with_returning(&self.db)
-            .await?;
-
-        Ok(result)
-    }
+    // #[allow(dead_code)]
+    // pub async fn move_objects(
+    //     self,
+    //     seat_ids: Vec<Uuid>,
+    //     delta_x: f64,
+    //     delta_y: f64,
+    //     delta_z: f64,
+    // ) -> Result<Vec<event_object_position::Model>, AppError> {
+    //     let result = event_object_position::Entity::update_many()
+    //         .col_expr(
+    //             event_object_position::Column::PositionX,
+    //             Expr::col(event_object_position::Column::PositionX).add(delta_x),
+    //         )
+    //         .col_expr(
+    //             event_object_position::Column::PositionY,
+    //             Expr::col(event_object_position::Column::PositionY).add(delta_y),
+    //         )
+    //         .col_expr(
+    //             event_object_position::Column::PositionZ,
+    //             Expr::col(event_object_position::Column::PositionZ).add(delta_z),
+    //         )
+    //         .filter(event_object_position::Column::Id.is_in(seat_ids))
+    //         .exec_with_returning(&self.db)
+    //         .await?;
+    //
+    //     Ok(result)
+    // }
 }
